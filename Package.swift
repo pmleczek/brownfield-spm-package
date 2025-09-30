0d56d830de61cc879b77bd749ceb2d401537b3c6
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-09-30 12:21:56 +0200

    chore: add Package.swift and README

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "ReactNativeApp",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "ReactNativeApp",
+            targets: ["ReactNativeApp"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ReactNativeApp",
+            path: "./ReactNativeApp.xcframework"
+        )
+    ]
+)
+

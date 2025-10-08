97af4f01d7db311aeac12d4e20c4a4313e1c49ab
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-10-08 10:14:42 +0200

    chore: add ReactBrownfield to package targets

--- Package.swift
+++ Package.swift
@@ -17,6 +17,10 @@ let package = Package(
             name: "ReactNativeApp",
             path: "./ReactNativeApp.xcframework"
         )
+        .binaryTarget(
+            name: "ReactBrownfield",
+            path: "./ReactBrownfield.xcframework"
+        )
     ]
 )
 
